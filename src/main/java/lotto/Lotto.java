41ae1078f45c3f8b2ed806d5cb9667692bd2d69c
Dongnyoung <leedo7182@naver.com>
2025-11-01 07:57:07 +0900

    feat: 예외처리 메소드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplication(numbers);
+        checkRange(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +21,21 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void checkDuplication(List<Integer> numbers) {
+        //당첨번호 중복 확인
+        Set<Integer> numberSet = new HashSet<>();
+        for(int number : numbers){
+            if(!numberSet.add(number)){ //add가 false면 이미 존재
+                throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있음");
+            }
+        }
+    }
+    private void checkRange(List<Integer> numbers) {
+        //당첨번호 숫자범위 체크
+        for(int number : numbers){
+            if(number>45 || number<1){ //1~45 사이 숫자인지 체크
+                throw new IllegalArgumentException("[ERROR] 로또 번호가 숫자범위(1~45)를 벗어남");
+            }
+        }
+    }
 }
