5099cf8f51b10c063157eddad1b9141bccebd588
Dongnyoung <leedo7182@naver.com>
2025-11-01 07:57:33 +0900

    test: 로또 번호 숫자범위 벗어날 시 테스트코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -22,4 +22,10 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 숫자범위(1~45)를 벗어나면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_숫자범위를_벗어나면_예외가_발생한다(){
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5,46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
