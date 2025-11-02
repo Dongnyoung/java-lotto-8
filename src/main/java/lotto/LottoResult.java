bb7f9e2f5c8e77b8d27236d1876b179040144ab0
Dongnyoung <leedo7182@naver.com>
2025-11-03 00:03:01 +0900

    feat: LottoResult클래스로 로직분리

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public class LottoResult {
+    String description;
+    int prize;
+    int count;
+
+    LottoResult(String description, int prize, int count) {
+        this.description = description;
+        this.prize = prize;
+        this.count = count;
+    }
+}
